20d6b8f20218a1701ccc5b17a9c22f3001149d70
VStorm <tranvubao2004@gmail.com>
2025-08-18 17:17:45 +0700

    add views ejs

--- HoiDanIT/server.js
+++ HoiDanIT/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const path = require('path');
+
 const app = express();
 const port = 8080;
 
+// config template engine (EJS)
+// app.set('views', './src/views');
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
-    res.send("asdfasdf");
+    res.render('sample.ejs')
 });
 
 app.listen(port, () => {
