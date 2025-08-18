a4dfb9db2aa2c9fcdbb07245690789d8ced0e04a
VStorm <tranvubao2004@gmail.com>
2025-08-18 17:29:26 +0700

    add env

--- HoiDanIT/server.js
+++ HoiDanIT/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
+const hostname = process.env.HOST_NAME || 'localhost';
 
 // config template engine (EJS)
 // app.set('views', './src/views');
@@ -13,6 +15,6 @@ app.get("/", (req, res) => {
     res.render('sample.ejs')
 });
 
-app.listen(port, () => {
+app.listen(port, hostname, () => {
     console.log(`example app listening on port ${port}`)
 });
