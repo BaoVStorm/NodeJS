4a32a392fd0536e78e0cf214de89f15fe738334a
VStorm <tranvubao2004@gmail.com>
2025-08-19 17:55:19 +0700

    connect to MYSQL

--- HoiDanIT/server.js
+++ HoiDanIT/server.js
@@ -16,6 +16,26 @@ configViewEngine(app);
 app.use('/', webRoutes);
 app.use('/v2', webRoutes);
 
+// test connection
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'vstorm',
+    port: 3307, // default: 3306
+});
+
+// simple query
+connection.query(
+    'SELECT * FROM Users',
+    function (err, results, fields) {
+        console.log(">>> results: ", results); 
+        console.log(">>> fields: ", fields); 
+    }
+);
+
 app.listen(port, hostname, () => {
     console.log(`example app listening on port ${port}`)
 });
